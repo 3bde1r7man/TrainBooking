93c7e750c64bb3a49a4261fcd70d15d835858ca4
Seif <SE.SeifIbrahim@gmail.com>
2023-05-20 17:38:44 +0300

    Edited Trip

--- Src/Trip.py
+++ Src/Trip.py
@@ -3,39 +3,40 @@ import sqlite3
 import datetime
 class Trip():
     def __init__(self, tripId = None):
-        self.src 
-        self.dest
-        self.departs
-        self.arrives 
-        self.price
+        self.src = None
+        self.dest = None
+        self.departs = None
+        self.arrives = None
+        self.price = None
         self.tripId = tripId
         if(tripId != None):
             conn = sqlite3.connect('db.sqlite3')
             cursor = conn.cursor()
-            cursor.execute(f"SELECT * FROM Trip WHERE id = {tripId}")
+            cursor.execute(f"SELECT * FROM Trip WHERE tripId = {tripId}")
             data = cursor.fetchall()
-            self.src = data[0]
-            self.dest = data[1]
-            self.departs = data[2]
-            self.arrives = data[3]
-            self.price = data[4]
+            first_row = data[0]
+            self.src = first_row[0]
+            self.dest = first_row[1]
+            self.departs = first_row[2]
+            self.arrives = first_row[3]
+            self.price = first_row[4]
 
     def add_trip_to_database(self, adminId, trainId):
         conn = sqlite3.connect('db.sqlite3')
         cursor = conn.cursor()
-        query = "INSERT INTO Trip (src, dist, departs, arrives, price, adminId, trainId) VALUES (?, ?, ?, ?, ?, ?, ?)"
+        query = "INSERT INTO Trip (src, dest, departs, arrives, price, adminId, trainId) VALUES (?, ?, ?, ?, ?, ?, ?)"
         values = (self.src, self.dest, self.departs, self.arrives, self.price, adminId, trainId)
 
         cursor.execute(query, values)
         conn.commit()
         conn.close()
 
-    def update_trip_to_database(self, tripId):
+    def update_trip_to_database(self):
         conn = sqlite3.connect('db.sqlite3')
         cursor = conn.cursor()
-        query = "UPDATE Trip SET src=?, dist=?, departs=?, arrives=?, price=? WHERE tripId=?"  # Replace <condition> with the appropriate condition for your update
+        query = "UPDATE Trip SET src=?, dest=?, departs=?, arrives=?, price=? WHERE tripId=?"  # Replace <condition> with the appropriate condition for your update
 
-        values = (self.src, self.dest, self.departs, self.arrives, self.price, tripId)
+        values = (self.src, self.dest, self.departs, self.arrives, self.price, self.tripId)
 
         cursor.execute(query, values)
         conn.commit()
