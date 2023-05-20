da6e89944c21d7a6eacbace628b2c4b0165f84fe
Seif <SE.SeifIbrahim@gmail.com>
2023-05-20 17:52:50 +0300

    Changed Trip

--- Src/Trip.py
+++ Src/Trip.py
@@ -13,7 +13,7 @@ class Trip():
             conn = sqlite3.connect('db.sqlite3')
             cursor = conn.cursor()
             cursor.execute(f"SELECT * FROM Trip WHERE id = {tripId}")
-            data = cursor.fetchall()
+            data = cursor.fetchone()
             self.src = data[0]
             self.dest = data[1]
             self.departs = data[2]
@@ -23,19 +23,19 @@ class Trip():
     def add_trip_to_database(self, adminId, trainId):
         conn = sqlite3.connect('db.sqlite3')
         cursor = conn.cursor()
-        query = "INSERT INTO Trip (src, dist, departs, arrives, price, adminId, trainId) VALUES (?, ?, ?, ?, ?, ?, ?)"
+        query = "INSERT INTO Trip (src, dest, departs, arrives, price, adminId, trainId) VALUES (?, ?, ?, ?, ?, ?, ?)"
         values = (self.src, self.dest, self.departs, self.arrives, self.price, adminId, trainId)
 
         cursor.execute(query, values)
         conn.commit()
         conn.close()
 
-    def update_trip_to_database(self, tripId):
+    def update_trip_to_database(self):
         conn = sqlite3.connect('db.sqlite3')
         cursor = conn.cursor()
-        query = "UPDATE Trip SET src=?, dist=?, departs=?, arrives=?, price=? WHERE tripId=?"  # Replace <condition> with the appropriate condition for your update
+        query = "UPDATE Trip SET src=?, dest=?, departs=?, arrives=?, price=? WHERE tripId=?"  # Replace <condition> with the appropriate condition for your update
 
-        values = (self.src, self.dest, self.departs, self.arrives, self.price, tripId)
+        values = (self.src, self.dest, self.departs, self.arrives, self.price, self.tripId)
 
         cursor.execute(query, values)
         conn.commit()
