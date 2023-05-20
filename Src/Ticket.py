9de0dfc6def36cafa8291406d40a96b5838c573b
nbow2 <117636018+nbow2@users.noreply.github.com>
2023-05-20 13:35:06 +0300

    Update Ticket.py
    ?

--- Src/Ticket.py
+++ Src/Ticket.py
@@ -1,4 +1,29 @@
 import sqlite3
+import pyodbc
+import Customer
+import Trip
+import Class
 
-conn = sqlite3.connect('db.sqlite3')
-cursor = conn.cursor()
+# by ahmed 
+#conn = sqlite3.connect('db.sqlite3')
+#cursor = conn.cursor()
+
+class Ticket():
+    
+    def __init__(self):
+      self.ticketId = ""
+
+
+      def add_Ticket_to_database(self.TicketId):
+          conn = sqlite3.connect('db.sqlite3')
+          cursor = conn.cursor()
+
+
+           AAA = "INSERT INTO Ticket (ticketId ,customerId ,tripId ,classId) VALUES (? , ? , ? , ? )"
+           iD = (self.ticketId, customerId, tripId ,classId )
+
+
+           cursor.execute(AAA, iD)
+        conn.commit()
+        conn.close()
+
